ea3a649f9d96a9dbe74f351657df7bdb80c506b2
GUIGMA Safiyata <GUIGMA Safiyata@Ordinateur>
2024-10-02 15:51:58 +0200

    Etat initial Ajout des classes entités Ajout MustomerManager

--- src/main/java/com/mycompany/monprojetfacture/jsf/LoginBean.java
+++ src/main/java/com/mycompany/monprojetfacture/jsf/LoginBean.java
@@ -4,10 +4,48 @@
  */
 package com.mycompany.monprojetfacture.jsf;
 
-/**
- *
- * @author GUIGMA Safiyata
- */
-public class LoginBean {
-    
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import java.io.Serializable;
+
+@Named("loginBean")
+@SessionScoped
+public class LoginBean implements Serializable {
+
+    private static final long serialVersionUID = 1L; // Ajout d'un identifiant de version pour la sérialisation
+
+    private String login = "safi";
+    private String password = "002";
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        System.out.println("in setLogin with" + login);
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        System.out.println("in setpassword with" + password);
+        this.password = password;
+    }
+
+    public String returnAction() {
+//        // Logique pour vérifier les identifiants, par exemple
+//        if (login.equals("safi@gmail.fr") && password.equals("002")) {
+//            // Authentification réussie
+//            FacesContext.getCurrentInstance().addMessage(null,
+//                    new FacesMessage("Connexion réussie!"));
+//            // Redirige vers la page suivante
+//        }
+        return "pagesuivante"; 
+    }
+
 }
